8ca58208dfaaba97a50bce4b4fef0015ecb8da36
Tom Vander Aa <vanderaa@imec.be>
2020-01-23 18:18:28 +0100

    ENH: flt -> float_type

--- cpp/SmurffCpp/Types.h
+++ cpp/SmurffCpp/Types.h
@@ -4,10 +4,10 @@
 #include <Eigen/Sparse>
 
 namespace smurff {
-   typedef double flt;
-   typedef Eigen::Matrix<flt, Eigen::Dynamic, Eigen::Dynamic> Matrix;
-   typedef Eigen::Matrix<flt, Eigen::Dynamic, 1> Vector;
-   typedef Eigen::Array<flt, Eigen::Dynamic, Eigen::Dynamic> Array2D;
-   typedef Eigen::Array<flt, Eigen::Dynamic, 1> Array1D;
-   typedef Eigen::SparseMatrix<flt> SparseMatrix;
+   typedef double float_type;
+   typedef Eigen::Matrix<float_type, Eigen::Dynamic, Eigen::Dynamic> Matrix;
+   typedef Eigen::Matrix<float_type, Eigen::Dynamic, 1> Vector;
+   typedef Eigen::Array<float_type, Eigen::Dynamic, Eigen::Dynamic> Array2D;
+   typedef Eigen::Array<float_type, Eigen::Dynamic, 1> Array1D;
+   typedef Eigen::SparseMatrix<float_type> SparseMatrix;
 };
