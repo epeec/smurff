fbd77943b2b6cff76da0856bc5bf122f47ca6244
Tom Vander Aa <vanderaa@imec.be>
2020-02-03 13:22:20 +0100

    FIX: APPROX_EPSILON: avoid ifdef

--- cpp/SmurffCpp/Types.h
+++ cpp/SmurffCpp/Types.h
@@ -6,13 +6,11 @@
 namespace smurff {
    typedef SMURFF_FLOAT_TYPE float_type;
 
-#if SMURFF_FLOAT_TYPE == float
-   const double APPROX_EPSILON = 0.01; // allow 1% difference
-#elif SMURFF_FLOAT_TYPE == double
-   const double APPROX_EPSILON = std::numeric_limits<float>::epsilon()*100;
-#else
-   const double APPROX_EPSILON = std::numeric_limits<SMURFF_FLOAT_TYPE>::epsilon()*100;
-#endif
+   template<typename F> double approx_epsilon();
+   template<> inline double approx_epsilon<float> () { return 0.01; }
+   template<> inline double approx_epsilon<double> () { return std::numeric_limits<float>::epsilon()*100; }
+
+#define APPROX_EPSILON (smurff::approx_epsilon<smurff::float_type>())
 
    typedef Eigen::Matrix<float_type, Eigen::Dynamic, Eigen::Dynamic> Matrix;
    typedef Eigen::Matrix<float_type, Eigen::Dynamic, 1> Vector;
