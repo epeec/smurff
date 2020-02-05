2e88561cafb7a2932245ea78dc8c5e15cb875131
Tom Vander Aa <vanderaa@imec.be>
2020-02-05 11:21:06 +0100

    WIP: cleanup find_boost

--- cmake/DependenciesConfig.cmake
+++ cmake/DependenciesConfig.cmake
@@ -119,64 +119,25 @@ macro(configure_eigen)
 endmacro(configure_eigen)
 
 macro(configure_boost)
+  message ("Dependency check for boost...")
   if(${ENABLE_BOOST})
-      message ("Dependency check for boost...")
-      
-      set (Boost_USE_STATIC_LIBS OFF)
-      set (Boost_USE_MULTITHREADED ON)
-
       # find boost random library - optional
-
-      if(${ENABLE_BOOST_RANDOM})
-        set (BOOST_COMPONENTS random)
-
-        FIND_PACKAGE(Boost COMPONENTS ${BOOST_COMPONENTS})
-
-        if(Boost_FOUND)
-            message("Found Boost random library")
-            message("Found Boost_VERSION: ${Boost_VERSION}")
-            message("Found Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
-            message("Found Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS}")
-
-            set(BOOST_RANDOM_LIBRARIES ${Boost_LIBRARIES})
-            set(BOOST_RANDOM_INCLUDE_DIRS ${Boost_INCLUDE_DIRS})
-
-            add_definitions(-DUSE_BOOST_RANDOM)
-            message("Boost minor: ${Boost_MINOR_VERSION}")
-            if ((${Boost_MINOR_VERSION} LESS "60") AND (${Boost_MINOR_VERSION} GREATER_EQUAL "54"))
-                add_definitions(-DTEST_RANDOM_OK)
-                message("Enabling test cases that depend on Boost random 1.5x.y")
-            endif()
-        else()
-            message("Boost random library is not found")
-        endif()
+      if(ENABLE_BOOST_RANDOM)
+        set (BOOST_COMPONENTS random system program_options)
+        FIND_PACKAGE(Boost 1.59 EXACT COMPONENTS ${BOOST_COMPONENTS} REQUIRED)
+        message("Found Boost random library")
+        add_definitions(-DUSE_BOOST_RANDOM)
+      else()
+        set (BOOST_COMPONENTS system program_options)
+        FIND_PACKAGE(Boost COMPONENTS ${BOOST_COMPONENTS} REQUIRED)
       endif()
 
-      #find boost program_options library - required
-
-      set (BOOST_COMPONENTS system 
-                            program_options)
-
-      FIND_PACKAGE(Boost COMPONENTS ${BOOST_COMPONENTS} REQUIRED)
-      
-      #see https://stackoverflow.com/questions/28887680/linking-boost-library-with-boost-use-static-lib-off-on-windows
-      if (MSVC)
-         # disable autolinking in boost
-         add_definitions(-DBOOST_ALL_NO_LIB)
-
-         # force all boost libraries to dynamic link (we already disabled
-         # autolinking, so I don't know why we need this, but we do!)
-         add_definitions(-DBOOST_ALL_DYN_LINK)
-      endif()
-  endif()
-
-  if(Boost_FOUND)
-      message("Found Boost_VERSION: ${Boost_VERSION}")
-      message("Found Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
-      message("Found Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS}")
+      message("-- Found Boost_VERSION: ${Boost_VERSION}")
+      message("-- Found Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
+      message("-- Found Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS}")
       add_definitions(-DHAVE_BOOST)
   else()
-      message("Boost library is not found")
+      message("-- Boost library is not enabled")
   endif()
 endmacro(configure_boost)
 
