7efaf866886edea0adfc3f4181e72dd820b7c15d
Tom Vander Aa <vanderaa@imec.be>
2020-02-18 10:03:23 +0100

    WIP: fix ini-file format

--- data/fix_configfiles.sh
+++ data/fix_configfiles.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+files=`find . -name '*ini'`
 
-for file in $*
+for file in $files
 do
     echo $file ...
     vim -e - $file <<@@@
 g/macau_prior_config_item/d
-g/\[side_info_/d
+g/side_info_\d_\d/d
+%s/macau_prior_config_/side_info_/g
 wq
 @@@
 
