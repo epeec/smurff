e486df25c555fafa93cb1366bab3f2c93f2af8fe
Tom Vander Aa <vanderaa@imec.be>
2020-02-21 11:08:32 +0100

    WIP: SparseTensor iterator like Eigen::SparseMatrix

---
+++ cpp/Utils/SparseTensor.cpp
@@ -0,0 +1,25 @@
+#include "SparseTensor.h"
+
+#include <numeric>
+
+#include <SmurffCpp/Utils/PVec.hpp>
+
+namespace smurff {
+
+std::pair<PVec<>, double> SparseTensor::get(std::uint64_t pos) const
+{
+   double val = m_values.at(pos);
+   PVec<> coords(getNModes());
+   for (unsigned j = 0; j < getNModes(); ++j)
+         coords[j] = m_columns[j][pos];
+
+   return std::make_pair(PVec<>(coords), val);
+}
+
+void SparseTensor::set(std::uint64_t pos, PVec<> coords, double value)
+{
+    m_values[pos] = value;
+    for(unsigned j=0; j<getNModes(); ++j) m_columns[j][pos] = coords[j];
+}
+
+} // end namespace smurff
--- cpp/Utils/SparseTensor.h
+++ cpp/Utils/SparseTensor.h
@@ -6,6 +6,8 @@
 #include <cstdint>
 #include <algorithm>
 
+#include <SmurffCpp/Utils/PVec.hpp>
+
 namespace smurff
 {
    struct SparseTensor
@@ -15,10 +17,17 @@ namespace smurff
       columns_type               m_columns;
       std::vector<double>        m_values;
 
+      int getNModes() const { return m_dims.size(); }
+      const std::vector<std::uint64_t> & getDims() { return m_dims; };
+      std::uint64_t getNNZ() const { return m_values.size(); }
+
       const std::vector<double>& getValues() const { return m_values; }
       const std::vector<std::uint32_t>& getColumn(int i) const { return m_columns.at(i); }
 
       std::vector<double>& getValues() { return m_values; }
       std::vector<std::uint32_t>& getColumn(int i)  { return m_columns.at(i); }
+
+      std::pair<PVec<>, double> get(std::uint64_t) const;
+      void set(std::uint64_t, PVec<>, double);
    };
 }
