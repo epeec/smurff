9ec9a903a37be323d53d05de21f9fa105e24ee1c
Tom Vander Aa <vanderaa@imec.be>
2020-02-25 08:39:04 +0100

    WIP: test DenseTensor and SparseTensor i.o. TensorConfig

--- cpp/Tests/TestsTensorConfig.cpp
+++ cpp/Tests/TestsTensorConfig.cpp
@@ -11,17 +11,15 @@
 
 #include <SmurffCpp/Utils/MatrixUtils.h>
 #include <SmurffCpp/Utils/TensorUtils.h>
-#include <SmurffCpp/Configs/TensorConfig.h>
+#include <Utils/Tensor.h>
 
 namespace smurff {
 
-static NoiseConfig fixed_ncfg(NoiseTypes::fixed);
-
-TEST_CASE("TensorConfig(const std::vector<std::uint64_t>& dims, const std::vector<double> values, const NoiseConfig& noiseConfig)")
+TEST_CASE("DenseTensor(const std::vector<std::uint64_t>& dims, const std::vector<double> values)")
 {
    std::vector<std::uint64_t> tensorConfigDims = { 3, 4 };
    std::vector<double> tensorConfigValues = { 1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12 };
-   TensorConfig tensorConfig(tensorConfigDims, tensorConfigValues.data(), fixed_ncfg);
+   DenseTensor tensorConfig(tensorConfigDims, tensorConfigValues);
 
    Matrix actualMatrix = matrix_utils::dense_to_eigen(tensorConfig);
    Matrix expectedMatrix(3, 4);
@@ -30,7 +28,7 @@ TEST_CASE("TensorConfig(const std::vector<std::uint64_t>& dims, const std::vecto
    REQUIRE(matrix_utils::equals(actualMatrix, expectedMatrix));
 }
 
-TEST_CASE("TensorConfig(const std::vector<std::uint64_t>& dims, const std::vector<std::vector<std::uint32_t>>& columns, const std::vector<double>& values, const NoiseConfig& noiseConfig)")
+TEST_CASE("SparseTensor(const std::vector<std::uint64_t>& dims, const std::vector<std::vector<std::uint32_t>>& columns, const std::vector<double>& values)")
 {
    std::vector<std::uint64_t> tensorConfigDims = { 3, 4 };
    std::vector<std::vector<std::uint32_t>> tensorConfigColumns = {
@@ -38,7 +36,7 @@ TEST_CASE("TensorConfig(const std::vector<std::uint64_t>& dims, const std::vecto
       { 0, 1, 2, 3, 0, 1, 2, 3 }
    };
    std::vector<double> tensorConfigValues = { 1, 2, 3, 4, 9, 10, 11, 12 };
-   TensorConfig tensorConfig(tensorConfigDims, tensorConfigColumns, tensorConfigValues, fixed_ncfg, false);
+   SparseTensor tensorConfig(tensorConfigDims, tensorConfigColumns, tensorConfigValues);
 
    SparseMatrix actualMatrix = matrix_utils::sparse_to_eigen(tensorConfig);
    SparseMatrix expectedMatrix(3, 4);
@@ -51,31 +49,6 @@ TEST_CASE("TensorConfig(const std::vector<std::uint64_t>& dims, const std::vecto
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 1, 10));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 2, 11));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 3, 12));
-   expectedMatrix.setFromTriplets(expectedMatrixTriplets.begin(), expectedMatrixTriplets.end());
-
-   REQUIRE(matrix_utils::equals(actualMatrix, expectedMatrix));
-}
-
-TEST_CASE("TensorConfig(const std::vector<std::uint64_t>& dims, const std::vector<std::vector<std::uint32_t>>& columns, const NoiseConfig& noiseConfig)")
-{
-   std::vector<std::uint64_t> tensorConfigDims = { 3, 4 };
-   std::vector<std::vector<std::uint32_t>> tensorConfigColumns = { 
-      { 0, 0, 0, 0, 2, 2, 2, 2 },
-      { 0, 1, 2, 3, 0, 1, 2, 3 }
-   };
-   TensorConfig tensorConfig(tensorConfigDims, tensorConfigColumns, fixed_ncfg, false);
-
-   SparseMatrix actualMatrix = matrix_utils::sparse_to_eigen(tensorConfig);
-   SparseMatrix expectedMatrix(3, 4);
-   std::vector<Eigen::Triplet<double> > expectedMatrixTriplets;
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 0, 1));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 1, 1));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 2, 1));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 3, 1));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 0, 1));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 1, 1));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 2, 1));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 3, 1));
    expectedMatrix.setFromTriplets(expectedMatrixTriplets.begin(), expectedMatrixTriplets.end());
 
    REQUIRE(matrix_utils::equals(actualMatrix, expectedMatrix));
