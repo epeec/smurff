f6424b2a221cc8356cd93c3a9fa6f9d20f0fb336
Tom Vander Aa <vanderaa@imec.be>
2020-02-28 15:22:26 +0100

    ENH: move boost/property_tree.hpp include to cpp

--- cpp/SmurffCpp/IO/INIFile.cpp
+++ cpp/SmurffCpp/IO/INIFile.cpp
@@ -4,10 +4,15 @@
 
 #include "INIFile.h"
 
+#include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
 
 namespace smurff
 {
+
+INIFile::INIFile() : m_tree(std::make_unique<pt::ptree>()) {}
+
+INIFile::~INIFile() {}
 
 pt::ptree INIFile::makeUnique(const pt::ptree &pt)
 {
@@ -41,19 +46,19 @@ void INIFile::read(const std::string &filename)
 {
    std::ifstream file;
    file.open(filename, std::ios::in);
-   pt::ini_parser::read_ini(file, m_tree);
+   pt::ini_parser::read_ini(file, *m_tree);
 }
 
 void INIFile::write(const std::string &filename)
 {
    std::ofstream file;
    file.open(filename, std::ios::trunc);
-   pt::ini_parser::write_ini(file, makeUnique(m_tree));
+   pt::ini_parser::write_ini(file, makeUnique(*m_tree));
 }
 
 bool INIFile::hasSection(const std::string &name) const
 {
-   return m_tree.get_child_optional(name) != boost::none;
+   return m_tree->get_child_optional(name) != boost::none;
 }
 
 bool INIFile::hasDataSet(const std::string &name, const std::string &tag) const
@@ -61,48 +66,110 @@ bool INIFile::hasDataSet(const std::string &name, const std::string &tag) const
    return false;
 }
 
-void INIFile::read(const std::string& section, const std::string& tag, Vector &) const
+template <typename T>
+T INIFile::getInternal(const std::string &section, const std::string &name, const T &default_value) const
+{
+   return m_tree->get<T>(section + "." + name, default_value);
+}
+
+int INIFile::get(const std::string &section, const std::string &tag, const int &default_value) const
+{
+   return getInternal(section, tag, default_value);
+}
+
+size_t INIFile::get(const std::string &section, const std::string &tag, const size_t &default_value) const
+{
+   return getInternal(section, tag, default_value);
+}
+
+double INIFile::get(const std::string &section, const std::string &tag, const double &default_value) const
+{
+   return getInternal(section, tag, default_value);
+}
+
+bool INIFile::get(const std::string &section, const std::string &tag, const bool &default_value) const
+{
+   return getInternal(section, tag, default_value);
+}
+
+std::string INIFile::get(const std::string &section, const std::string &tag, const std::string &default_value) const
+{
+   return getInternal(section, tag, default_value);
+}
+
+void INIFile::read(const std::string &section, const std::string &tag, Vector &) const
 {
    THROWERROR_NOTIMPL();
 }
 
-void INIFile::read(const std::string& section, const std::string& tag, Matrix &) const
+void INIFile::read(const std::string &section, const std::string &tag, Matrix &) const
 {
    THROWERROR_NOTIMPL();
 }
 
-void INIFile::read(const std::string& section, const std::string& tag, SparseMatrix &) const
+void INIFile::read(const std::string &section, const std::string &tag, SparseMatrix &) const
 {
    THROWERROR_NOTIMPL();
 }
 
-void INIFile::read(const std::string& section, const std::string& tag, DenseTensor &) const
+void INIFile::read(const std::string &section, const std::string &tag, DenseTensor &) const
 {
    THROWERROR_NOTIMPL();
 }
 
-void INIFile::read(const std::string& section, const std::string& tag, SparseTensor &) const
+void INIFile::read(const std::string &section, const std::string &tag, SparseTensor &) const
 {
    THROWERROR_NOTIMPL();
 }
 
-void INIFile::write(const std::string& section, const std::string& tag, const Vector &M)
+template <typename T>
+void INIFile::putInternal(const std::string &section, const std::string &tag, const T &value)
+{
+   m_tree->put(section + "." + tag, value);
+}
+
+void INIFile::put(const std::string &section, const std::string &tag, const int &value)
+{
+   putInternal(section, tag, value);
+}
+
+void INIFile::put(const std::string &section, const std::string &tag, const size_t &value)
+{
+   putInternal(section, tag, value);
+}
+
+void INIFile::put(const std::string &section, const std::string &tag, const double &value)
+{
+   putInternal(section, tag, value);
+}
+
+void INIFile::put(const std::string &section, const std::string &tag, const bool &value)
+{
+   putInternal(section, tag, value);
+}
+
+void INIFile::put(const std::string &section, const std::string &tag, const std::string &value)
+{
+   putInternal(section, tag, value);
+}
+
+void INIFile::write(const std::string &section, const std::string &tag, const Vector &M)
 {
 }
 
-void INIFile::write(const std::string& section, const std::string& tag, const Matrix &M)
+void INIFile::write(const std::string &section, const std::string &tag, const Matrix &M)
 {
 }
 
-void INIFile::write(const std::string& section, const std::string& tag, const SparseMatrix &X)
+void INIFile::write(const std::string &section, const std::string &tag, const SparseMatrix &X)
 {
 }
 
-void INIFile::write(const std::string& section, const std::string& tag, const DenseTensor &X)
+void INIFile::write(const std::string &section, const std::string &tag, const DenseTensor &X)
 {
 }
 
-void INIFile::write(const std::string& section, const std::string& tag, const SparseTensor &X)
+void INIFile::write(const std::string &section, const std::string &tag, const SparseTensor &X)
 {
 }
 
--- cpp/SmurffCpp/IO/INIFile.h
+++ cpp/SmurffCpp/IO/INIFile.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <string>
-#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ptree_fwd.hpp>
 
 namespace pt = boost::property_tree;
 
@@ -12,10 +12,11 @@ namespace smurff
 class INIFile : public ConfigFile
 {
 public:
-   ~INIFile() {}
+   INIFile();
+   ~INIFile();
 
 private:
-   pt::ptree m_tree;
+   std::unique_ptr<pt::ptree> m_tree;
    pt::ptree makeUnique(const pt::ptree &pt);
 
 public:
@@ -24,76 +25,31 @@ public:
 
 private:
    template <typename T>
-   T getInternal(const std::string &section, const std::string &name, const T &default_value) const
-   {
-      return m_tree.get<T>(section + "." + name, default_value);
-   }
+   T getInternal(const std::string &section, const std::string &name, const T &default_value) const;
 
    template <typename T>
-   void putInternal(const std::string &section, const std::string &tag, const T &value)
-   {
-      m_tree.put(section + "." + tag, value);
-   }
+   void putInternal(const std::string &section, const std::string &tag, const T &value);
 
 public:
    bool hasDataSet(const std::string &section, const std::string &tag) const override;
    bool hasSection(const std::string &section) const override;
 
-   int get(const std::string &section, const std::string &tag, const int &default_value) const override
-   {
-      return getInternal(section, tag, default_value);
-   }
-
-   size_t get(const std::string &section, const std::string &tag, const size_t &default_value) const override
-   {
-      return getInternal(section, tag, default_value);
-   }
-
-   double get(const std::string &section, const std::string &tag, const double &default_value) const override
-   {
-      return getInternal(section, tag, default_value);
-   }
-
-   bool get(const std::string &section, const std::string &tag, const bool &default_value) const override
-   {
-      return getInternal(section, tag, default_value);
-   }
-
-   std::string get(const std::string &section, const std::string &tag, const std::string &default_value) const override
-   {
-      return getInternal(section, tag, default_value);
-   }
-
+   int get(const std::string &section, const std::string &tag, const int &default_value) const override;
+   size_t get(const std::string &section, const std::string &tag, const size_t &default_value) const override;
+   double get(const std::string &section, const std::string &tag, const double &default_value) const override;
+   bool get(const std::string &section, const std::string &tag, const bool &default_value) const override;
+   std::string get(const std::string &section, const std::string &tag, const std::string &default_value) const override;
    void read(const std::string &section, const std::string &tag, Vector &) const override;
    void read(const std::string &section, const std::string &tag, Matrix &) const override;
    void read(const std::string &section, const std::string &tag, SparseMatrix &) const override;
    void read(const std::string &section, const std::string &tag, DenseTensor &) const override;
    void read(const std::string &section, const std::string &tag, SparseTensor &) const override;
 
-   void put(const std::string &section, const std::string &tag, const int &value) override
-   {
-      putInternal(section, tag, value);
-   }
-
-   void put(const std::string &section, const std::string &tag, const size_t &value) override
-   {
-      putInternal(section, tag, value);
-   }
-
-   void put(const std::string &section, const std::string &tag, const double &value) override
-   {
-      putInternal(section, tag, value);
-   }
-
-   void put(const std::string &section, const std::string &tag, const bool &value) override
-   {
-      putInternal(section, tag, value);
-   }
-
-   void put(const std::string &section, const std::string &tag, const std::string &value) override
-   {
-      putInternal(section, tag, value);
-   }
+   void put(const std::string &section, const std::string &tag, const int &value) override;
+   void put(const std::string &section, const std::string &tag, const size_t &value) override;
+   void put(const std::string &section, const std::string &tag, const double &value) override;
+   void put(const std::string &section, const std::string &tag, const bool &value) override;
+   void put(const std::string &section, const std::string &tag, const std::string &value) override;
 
    void write(const std::string &section, const std::string &tag, const Vector &) override;
    void write(const std::string &section, const std::string &tag, const Matrix &) override;
