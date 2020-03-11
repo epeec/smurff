8dc6682d925a8bf18ca789ec894d835f3bd85116
Tom Vander Aa <vanderaa@imec.be>
2020-03-11 16:36:00 +0100

    WIP: import as before in __init__

--- python/smurff/smurff/__init__.py
+++ python/smurff/smurff/__init__.py
@@ -1,2 +1,8 @@
 from .trainsession import TrainSession, Config
 from .helper import FixedNoise, SampledNoise, AdaptiveNoise, ProbitNoise
+from .prepare import make_train_test, make_train_test_df
+from .result import Prediction, calc_rmse, calc_auc
+from .predict import PredictSession
+from .datasets import load_chembl
+from .center import center_and_scale
+from .smurff import *
