eb0acbc54c352cc96fe9ae3e6c30a116147cdd64
Tom Vander Aa <vanderaa@imec.be>
2020-03-19 14:49:52 +0100

    FIX: squeeze mu

--- python/smurff/src/smurff/predict.py
+++ python/smurff/src/smurff/predict.py
@@ -64,7 +64,8 @@ class Sample:
                 # if side_info was specified for this dimension, we predict for this side_info
                 try:  # try to compute sideinfo * beta using dot
                     # compute latent vector from side_info
-                    uhat = c.dot(self.betas[m]).transpose()
+                    uhat = c.dot(self.betas()[m]).transpose()
+                    mu = np.squeeze(mu)
                     uhat = np.squeeze(uhat) 
                     operands += [uhat + mu, [0]]
                 except AttributeError:  # assume it is a coord
