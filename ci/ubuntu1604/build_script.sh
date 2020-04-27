f298f4820ba646f0e8da39713444170d0d5c55d0
Tom Vander Aa <vanderaa@imec.be>
2020-04-27 14:34:25 +0200

    ENH: fix docker script to take CPU_COUNT into account

--- ci/ubuntu1604/build_script.sh
+++ ci/ubuntu1604/build_script.sh
@@ -2,7 +2,7 @@
 #
 # Start this script in a Docker container like this:
 #
-#  docker run -v $(git rev-parse --show-toplevel):/smurff -ti smurff1604 /smurff/ci/ubuntu1604/build_script.sh
+#  docker run -eCPU_COUNT=2 -v $(git rev-parse --show-toplevel):/smurff -ti smurff1604 /smurff/ci/ubuntu1604/build_script.sh
 #
 # where smurff1604 is the image name
 
