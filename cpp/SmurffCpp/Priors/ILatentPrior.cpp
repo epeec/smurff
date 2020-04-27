c497f9457b5c8afbdd3734d630db19715767f477
Tom Vander Aa <vanderaa@imec.be>
2020-04-27 14:35:45 +0200

    Revert "FIX: make OpenMP deterministic"
    This reverts commit b40ab3e963f2bb7078572a2c94e3d9bfd9bb5252.

--- cpp/SmurffCpp/Priors/ILatentPrior.cpp
+++ cpp/SmurffCpp/Priors/ILatentPrior.cpp
@@ -105,7 +105,6 @@ void ILatentPrior::sample_latents()
    COUNTER("sample_latents");
    data().update_pnm(model(), m_mode);
 
-#pragma omp parallel for
    for (int n = 0; n < U().rows(); n++)
 #pragma omp task
    {
@@ -118,6 +117,7 @@ void ILatentPrior::sample_latents()
       if (m_session.inSamplingPhase())
          model().updateAggr(m_mode, n);
    }
+#pragma omp taskwait
 
    if (m_session.inSamplingPhase())
       model().updateAggr(m_mode);
