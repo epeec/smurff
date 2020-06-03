614e30410dbbdd40fbc5bbb73c6816031b66bae3
Tom Vander Aa <vanderaa@imec.be>
2020-06-03 14:06:28 +0200

    WIP: smaller Docker image for building Linux wheels

--- build_wheels/build_linux_wheels.sh
+++ build_wheels/build_linux_wheels.sh
@@ -7,8 +7,8 @@ cd python/smurff
 
 for PYVER in "cp35-cp35m" "cp36-cp36m" "cp37-cp37m" "cp38-cp38" ; do
   PYPREFIX="/opt/python/${PYVER}"
-  CMAKE_ARGS="-DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169
-     -DPYTHON_INCLUDE_DIR=${PYPREFIX}/include"
+  PYINCLUDE_DIR=$(echo ${PYPREFIX}/include/python*)
+  CMAKE_ARGS="-DENABLE_BOOST=OFF -DPYTHON_INCLUDE_DIR=${PYINCLUDE_DIR}"
   "${PYPREFIX}/bin/pip" install Cython numpy 
   "${PYPREFIX}/bin/python" setup.py --extra-cmake-args "$CMAKE_ARGS" bdist_wheel
 done
